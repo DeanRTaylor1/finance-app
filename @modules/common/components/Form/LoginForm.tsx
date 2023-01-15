2162907c035aa441dd1904871f0728ea17e09a4f
deanrtaylor1 <deanrtaylor@hotmail.com>
2023-01-15 15:20:52 +0700

    added login with facebook/github

--- @modules/common/components/Form/LoginForm.tsx
+++ @modules/common/components/Form/LoginForm.tsx
@@ -4,7 +4,8 @@ import { useState } from 'react';
 import DoRequest from '@modules/common/hooks/do-request';
 import Formerrors from './Form-Errors';
 import Input from './Input';
-
+import { FcGoogle } from 'react-icons/fc';
+import { BsFacebook, BsGithub } from 'react-icons/bs';
 import GoogleButton from 'react-google-button';
 
 const LoginForm: React.FC = () => {
@@ -51,6 +52,45 @@ const LoginForm: React.FC = () => {
       }, 500);
     }
   };
+  const redirectToFacebook = async () => {
+    let timer: NodeJS.Timer | null = null;
+    const facebookLoginURL = `${process.env.NEXT_PUBLIC_API_URL}api/login/facebook`;
+    const newWindow = window.open(
+      facebookLoginURL,
+      '_blank',
+      'width=500, height=600'
+    );
+    if (newWindow) {
+      timer = setInterval(() => {
+        if (newWindow.closed) {
+          if (timer) {
+            clearInterval(timer);
+          }
+          Router.push('/');
+        }
+      }, 500);
+    }
+  };
+
+  const oAuthLogin = async (strategy: string) => {
+    let timer: NodeJS.Timer | null = null;
+    const oAuthLoginURL = `${process.env.NEXT_PUBLIC_API_URL}api/login/${strategy}`;
+    const newWindow = window.open(
+      oAuthLoginURL,
+      '_blank',
+      'width=500, height=600'
+    );
+    if (newWindow) {
+      timer = setInterval(() => {
+        if (newWindow.closed) {
+          if (timer) {
+            clearInterval(timer);
+          }
+          Router.push('/');
+        }
+      }, 500);
+    }
+  };
   return (
     <div className='w-full h-full flex mt-14 items-start justify-center z-10'>
       <form
@@ -81,8 +121,22 @@ const LoginForm: React.FC = () => {
           <div className='w-full flex justify-center items-center font-light text-xs'>
             OR
           </div>
-          <div className='flex justify-center items-center w-full pb-4'>
-            <GoogleButton type='light' onClick={redirectToGoogle} />
+          <div className='flex justify-center items-center w-full pb-4 gap-4'>
+            <FcGoogle
+              size={35}
+              className='hover:cursor-pointer hover:opacity-75'
+              onClick={() => oAuthLogin('github')}
+            />
+            <BsFacebook
+              size={30}
+              className='hover:cursor-pointer hover:opacity-75'
+              onClick={() => oAuthLogin('facebook')}
+            />
+            <BsGithub
+              size={30}
+              className='hover:cursor-pointer hover:opacity-75'
+              onClick={() => oAuthLogin('github')}
+            />
           </div>
         </div>
 
